b959a639ac13f9adc315add69a8135beae7d3948
François Lyons <lyons.fr55@gmail.com>
2022-10-29 20:52:35 +0200

    change1

--- src/components/test-component/test.component.jsx
+++ src/components/test-component/test.component.jsx
@@ -2,54 +2,47 @@ import "./test.styles.scss"
 
 
 const TestComponent = () =>{
-    // const [inputValue, setInputValue] = useState('')
-   
-
-    // const inputHandler =(event) =>{
-    //     const string = event.target.value;
-    //     setInputValue(string);
-    // }
-
-    // const wordsMemo = useMemo(()=>{
-    //     return [words, setWords] = useState([{str: 'A', color: 'color-one'}, {str: "B", color:"color-one"}, {str: "C", color: "color-one"}]);
-    // }, [])
-
-    // useEffect(()=>{
-    //     const charWords = wordsMemo.map(word => word.str);
-    //     const charInput = inputValue.charAt(inputValue.length-1);
-    //     if(charInput === charWords[inputValue.length-1]){
-    //         const goodAnswer = words.map((elem,i)=>{
-    //             if(i === inputValue.length-1){
-    //                 return {...elem, color: "color-two",}
-    //             }else{
-    //                 return elem;
-    //             }
-    //         })
-    //         setWords(goodAnswer);
-    //     }else{
-    //         if(charInput ===""){
-
-    //         }else{
-    //             const wrongAnswer = words.map((elem,i)=>{
-    //             if(i === inputValue.length-1){
-    //                 return {...elem, color: "wrong-answer",}
-    //             }else{
-    //                 return elem;
-    //             }
-    //         })
-    //         setWords(wrongAnswer);
-    //         }
-    //     }
-    // }, [inputValue, words])
-
+    const [inputValue, setInputValue] = useState('');
+    const [words, setWords] = useState([{str: 'A', color: 'color-one'}, {str: "B", color:"color-one"}, {str: "C", color: "color-one"}]);
+    
+    const inputHandler =(event) =>{
+        const string = event.target.value;
+        setInputValue(string);
+    }
+    
+    useEffect(()=>{
+        const charWords = words.map(word => word.str);
+        const charInput = inputValue.charAt(inputValue.length-1);
+        if(charInput === charWords[inputValue.length-1]){
+            const goodAnswer = words.map((elem,i)=>{
+                if(i === inputValue.length-1){
+                    return {...elem, color: "color-two",}
+                }else{
+                    return elem;
+                }
+            })
+            setWords(goodAnswer);
+        }else{
+            if(charInput ===""){
+    
+            }else{
+                const wrongAnswer = words.map((elem,i)=>{
+                if(i === inputValue.length-1){
+                    return {...elem, color: "wrong-answer",}
+                }else{
+                    return elem;
+                }
+            })
+            setWords(wrongAnswer);
+            }
+        }
+    }, [inputValue]);
+    
     return(
         <div>
-        {/* {words.map((elem)=>(
-            <label className={elem.color}>{elem.str}</label>
-        ))}   
-        <input value={inputValue} onChange={inputHandler}/> */}
-                
+        
+                    
         </div>
     )
-}
-export default TestComponent;
+    }
+    export default TestComponent;
