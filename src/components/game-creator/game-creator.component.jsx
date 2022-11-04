a0711f4685455adbfb618e89a28817a8081cb295
François Lyons <lyons.fr55@gmail.com>
2022-11-04 17:39:50 +0100

    divided component

--- src/components/game-creator/game-creator.component.jsx
+++ src/components/game-creator/game-creator.component.jsx
@@ -1,9 +1,62 @@
+import './game-creator.styles.scss';
+import { useReducer, useEffect } from 'react';
 
+const reducer = (state, action) =>{
+    switch(action.type){
+        case "set-changes":
+            return action.payload;
+        case "good-answer":
+            return state.map((elem, i) =>{
+                if(i===action.id){
+                    return{...elem, color: "color-two"};
+                }else{
+                    return elem;
+                }
+            });
+            case "wrong-answer":
+            return state.map((elem,i) =>{
+                if(i === action.id){
+                    return{...elem, color: "color-three"};
+                }else{
+                    return elem;
+                }
+            });
+        default:
+            throw new Error("l'acion "+action.type + "n'existe pas");
+    }
+}
 
-const GameCreator = () =>{
+const GameCreator = ({letters, resetArray, inputValue}) =>{
+    const [updatedWords, dispatch] = useReducer(reducer, letters);
+
+    //updating state of the useReducer 
+    useEffect(()=>{
+        dispatch({type: "set-changes", payload:letters})
+     }, [letters]);
+ 
+     useEffect(()=>{
+        const charRandom = letters.map(elem =>elem.str);
+        const charInput = inputValue.charAt(inputValue.length-1);
+
+        if(charInput === charRandom[inputValue.length-1]){
+            dispatch({type: "good-answer", id: inputValue.length-1});
+        }else{
+            if(charInput.length ===0){
+                return;
+            }else{
+            dispatch({type: "wrong-answer", id:  inputValue.length-1});
+        }
+    }
+    }, [inputValue, letters]);
+
     return(
-        <div>
-            
+        <div className="char-nav">
+            <div className="charac-zone">
+                {updatedWords.map((elem, id) =>(
+                    <span className={`${elem.color} charac-list`} key={id}>{elem.str}</span>
+                ))}
+            </div>
+            <button className="button-ex1-style" onClick={resetArray}>Reset</button>
         </div>
     )
 }
--- src/components/level1/exo1.component.jsx
+++ src/components/level1/exo1.component.jsx
@@ -1,32 +1,8 @@
-import { useEffect, useState, useContext, useReducer} from "react";
+import { useEffect, useState, useContext} from "react";
 import { KeyBoardOneContext } from "../../context/keyboard-1-context/keyboard1-context.context";
+import GameCreator from "../game-creator/game-creator.component";
 import KeyboardOne from "../keyboards/keyboard-lelve1/keyboard-1.component";
 import './exo1.styles.scss';
-
-const reducer = (state, action) =>{
-    switch(action.type){
-        case "set-changes":
-            return action.payload;
-        case "good-answer":
-            return state.map((elem, i) =>{
-                if(i===action.id){
-                    return{...elem, color: "color-two"};
-                }else{
-                    return elem;
-                }
-            });
-            case "wrong-answer":
-            return state.map((elem,i) =>{
-                if(i === action.id){
-                    return{...elem, color: "color-three"};
-                }else{
-                    return elem;
-                }
-            });
-        default:
-            throw new Error("l'acion "+action.type + "n'existe pas");
-    }
-}
 
 
 const FirstExo = () =>{
@@ -36,18 +12,12 @@ const FirstExo = () =>{
     const [inputValue, setInputValue] = useState('');
     const keysList = ['Q','S','D','F','J','K','L','M'];
     const {setKeyActive} = useContext(KeyBoardOneContext);
-    const [updatedWords, dispatch] = useReducer(reducer, letters);
 
     const resetArray = () =>{
         setLetters([]);
         setInputValue("");
         setPlayButton(false);
     }
-
-    //updating state of the useReducer 
-    useEffect(()=>{
-       dispatch({type: "set-changes", payload:letters})
-    }, [letters]);
 
     const printRandom = () =>{
        resetArray();
@@ -64,22 +34,6 @@ const FirstExo = () =>{
         const str = event.target.value;
         setInputValue(str);
     };
-
-    
-    useEffect(()=>{
-        const charRandom = letters.map(elem =>elem.str);
-        const charInput = inputValue.charAt(inputValue.length-1);
-
-        if(charInput === charRandom[inputValue.length-1]){
-            dispatch({type: "good-answer", id: inputValue.length-1});
-        }else{
-            if(charInput.length ===0){
-                return;
-            }else{
-            dispatch({type: "wrong-answer", id:  inputValue.length-1});
-        }
-    }
-    }, [inputValue, letters]);
     
     useEffect(()=>{
         var charRandom =   letters.map(elem =>elem.str);
@@ -92,14 +46,7 @@ const FirstExo = () =>{
             <h1>qsdf - jklm</h1>
             <KeyboardOne />
             {playButton&& 
-            <div className="char-nav">
-                <div className="charac-zone">
-                    {updatedWords.map((elem, id) =>(
-                        <span className={`${elem.color} charac-list`} key={id}>{elem.str}</span>
-                    ))}
-                </div>
-                <button className="button-ex1-style" onClick={resetArray}>Reset</button>
-            </div>}
+            <GameCreator letters={letters} resetArray={resetArray} inputValue={inputValue}/>}
             {playButton?
             <input className="input-exo1" type="text" autoFocus onChange={inputListener} value={inputValue} />:
             <button className="button-ex1-style" type="submit" onClick={printRandom}>Start</button>}
